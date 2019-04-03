b4699c48efc7f7e417c3ff354bf4ac6e52f4cec7
Callum Smith <callum.smith256@gmail.com>
2019-04-03 09:43:58 +0000

    updated project

--- proj1.c
+++ proj1.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 
-int k = enc(int x);
+
 
 int main()
 {
-    char str[] = {a, b, c, d, e, f, g, h}
     
-    char a = 0;
-    char b = 1;
-    char c = 2;
-    char d = 3;
-    char e = 4;
-    char f = 5;
-    char g = 6;
-    char h = 7;
-    char i = 8;
-    char j = 9;
-    char k = 10;
-    char l = 11;
-    char m = 12;
-    char n = 13;
-    char o = 14;
-    char p = 15;
-    char q = 16;
-    char r = 17;
-    char s = 18;
-    char t = 19;
-    char u = 20;
-    char v = 21;
-    char w = 22;
-    char x = 23;
-    char y = 24;
-    char z = 25;
-    int k;
+    printf("Please select an option: \n");
+    printf("a) Start a new game\n");
+    printf("b) Load a saved game\n");
+    printf("c) Options\n");
+    printf("d) Quit\n");
+    printf("Selection: ");
+    
+    char c;
+    scanf("%c", &c);
+    
+    while(c < 'a' || c > 'd')
+    
+    switch(c)
+    {
+        case 'a': newGame();
+        break;
+        case 'b': loadGame();
+        break;
+        case 'c': options();
+        break;
+        case 'd': quit();
+        break;
+    }
+   
 
   return 0;
 }
 
-int k = enc(int x)
-{
-    
-}
