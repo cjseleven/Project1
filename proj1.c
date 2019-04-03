ce6cdcf3103c5ae486e70e666cb484951931ff73
Callum Smith <callum.smith256@gmail.com>
2019-04-03 07:13:58 +0000

    updated task

--- proj1.c
+++ proj1.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 
-int main
+int k = enc(int x)
+
+int main()
 {
-    hello
-    hello
-    hello
-    hello
+    
+    char a = 0;
+    char b = 1;
+    char c = 2;
+    char d = 3;
+    char e = 4;
+    char f = 5;
+    char g = 6;
+    char h = 7;
+    char i = 8;
+    char j = 9;
+    char k = 10;
+    char l = 11;
+    char m = 12;
+    char n = 13;
+    char o = 14;
+    char p = 15;
+    char q = 16;
+    char r = 17;
+    char s = 18;
+    char t = 19;
+    char u = 20;
+    char v = 21;
+    char w = 22;
+    char x = 23;
+    char y = 24;
+    char z = 25;
+    int k;
 
   return 0;
+}
+
+int k = enc(int x)
+{
+    
 }
